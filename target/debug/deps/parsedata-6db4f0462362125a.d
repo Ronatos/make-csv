addb6e6f74921019ed1feae6bd1a381a044259b1
Alexander L. Perez <alexander.l.perez@outlook.com>
2021-11-12 12:56:26 -0500

    Delete target/debug directory

--- target/debug/deps/parsedata-6db4f0462362125a.d
+++
@@ -1,5 +0,0 @@
-/home/n01413497/Documents/rust/parsedata/target/debug/deps/parsedata-6db4f0462362125a: src/main.rs
-
-/home/n01413497/Documents/rust/parsedata/target/debug/deps/parsedata-6db4f0462362125a.d: src/main.rs
-
-src/main.rs:
--- target/debug/parsedata.d
+++
@@ -1,1 +0,0 @@
-/home/n01413497/Documents/rust/parsedata/target/debug/parsedata: /home/n01413497/Documents/rust/parsedata/src/main.rs
